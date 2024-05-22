0fcc96f337e1481cc1251fff34ed364e00a54d0c
miandiee <emiadatitasinulingga@gmail.com>
2024-05-23 02:32:49 +0700

    last changes

--- js/script.js
+++ js/script.js
@@ -19,67 +19,114 @@ setInterval(() => {
   moveSlide(1);
 }, 7000);
 
+function validationArea() {
+   let baseValue = parseFloat(document.getElementById("base").value);
+   let heightValue = parseFloat(document.getElementById("height").value);
+ 
+   console.log(baseValue);
+   console.log(heightValue);
+ 
+   if (baseValue != "" && heightValue != "" && !isNaN(baseValue) && !isNaN(heightValue)) {
+     console.log("Validation Succeed");
+     let area = (baseValue * heightValue) / 2;
+     console.log(area);
+     document.getElementById("displayFormulaArea").textContent = `Area of Triangle: 0.5 x base x height`;
+     document.getElementById("displayValueArea").textContent = `Area of Triangle: 0.5 x ${baseValue} x ${heightValue}`;
+     document.getElementById("displayArea").textContent = `Area of Triangle: ${area}`;
+   } else {
+     console.log("Validation Failed");
+     alert("Input must be filled out with valid numbers");
+   }
+ }
+
+function validationPerimeter() {
+   let aValue = parseFloat(document.getElementById("side-a").value);
+   let bValue = parseFloat(document.getElementById("side-b").value);
+   let cValue = parseFloat(document.getElementById("side-c").value);
+ 
+   console.log(aValue);
+   console.log(bValue);
+   console.log(cValue);
+ 
+   if (aValue != "" && bValue != "" && cValue != "" && !isNaN(aValue) && !isNaN(bValue) && !isNaN(cValue)) {
+     console.log("Validation Succeed");
+     let perimeter = (aValue + bValue + cValue);
+     console.log(perimeter);
+     document.getElementById("displayFormulaPerimeter").textContent = `Perimeter of Triangle: a + b + c`;
+     document.getElementById("displayValuePerimeter").textContent = `Perimeter of Triangle: ${aValue} + ${bValue} + ${cValue}`;
+     document.getElementById("displayPerimeter").textContent = `Perimeter of Triangle: ${perimeter}`;
+   } else {
+     console.log("Validation Failed");
+     alert("Input must be filled out with valid numbers");
+   }
+ }
+ 
 document.getElementById("base").addEventListener('input', function displayChange() {
-  console.log(this.value)
-  let e = this.value
+  console.log(this.value);
+  let e = this.value;
   
-  let chars = /^[0-9]+$/
+  let chars = /^[0-9]+$/;
 
   if (chars.test(e)) {
-     console.log("digits")
+     console.log("digits");
   } else {
-     console.log("not digits")
-  }
+     console.log("not digits");
+     alert("Not Digits, Value Must be Positive!");
+   }
 })
 
 document.getElementById("height").addEventListener('input', function displayChange() {
-  console.log(this.value)
-  let e = this.value
-  
-  let chars = /^[0-9]+$/
-
-  if (chars.test(e)) {
-     console.log("digits")
-  } else {
-     console.log("not digits")
-  }
+   console.log(this.value);
+   let e = this.value;
+   
+   let chars = /^[0-9]+$/;
+   
+   if (chars.test(e)) {
+      console.log("digits");
+   } else {
+      console.log("not digits");
+      alert("Not Digits, Value Must be Positive!");
+   }
 })
 
 document.getElementById("side-a").addEventListener('input', function displayChange() {
-  console.log(this.value)
-  let e = this.value
-  
-  let chars = /^[0-9]+$/
-
-  if (chars.test(e)) {
-     console.log("digits")
-  } else {
-     console.log("not digits")
-  }
+   console.log(this.value);
+   let e = this.value;
+   
+   let chars = /^[0-9]+$/;
+   
+   if (chars.test(e)) {
+      console.log("digits");
+   } else {
+      console.log("not digits");
+      alert("Not Digits, Value Must be Positive!");
+   }
 })
 
 document.getElementById("side-b").addEventListener('input', function displayChange() {
-  console.log(this.value)
-  let e = this.value
-  
-  let chars = /^[0-9]+$/
-
-  if (chars.test(e)) {
-     console.log("digits")
-  } else {
-     console.log("not digits")
-  }
+   console.log(this.value);
+   let e = this.value;
+   
+   let chars = /^[0-9]+$/;
+   
+   if (chars.test(e)) {
+      console.log("digits");
+   } else {
+      console.log("not digits");
+      alert("Not Digits, Value Must be Positive!");
+   }
 })
 
 document.getElementById("side-c").addEventListener('input', function displayChange() {
-  console.log(this.value)
-  let e = this.value
-  
-  let chars = /^[0-9]+$/
-
-  if (chars.test(e)) {
-     console.log("digits")
-  } else {
-     console.log("not digits")
-  }
+   console.log(this.value);
+   let e = this.value;
+   
+   let chars = /^[0-9]+$/;
+   
+   if (chars.test(e)) {
+      console.log("digits");
+   } else {
+      console.log("not digits");
+      alert("Not Digits, Value Must be Positive!");
+   }
 })
