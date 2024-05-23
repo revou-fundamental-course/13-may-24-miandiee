dc5c165791e64fa057c93c8e24e8246a5c47e000
miandiee <emiadatitasinulingga@gmail.com>
2024-05-23 21:31:17 +0700

    penambahan comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// fungsi slider
 let currentIndex = 0;
 
 function moveSlide(direction) {
@@ -15,31 +16,50 @@ function moveSlide(direction) {
   slides.style.transform = `translateX(${offset}%)`;
 }
 
+// fungsi set timer interval slider
 setInterval(() => {
   moveSlide(1);
 }, 7000);
 
+
+// fungsi validasi luas
 function validationArea() {
+   // ambil dan konversi nilai base dan height
    let baseValue = parseFloat(document.getElementById("base").value);
    let heightValue = parseFloat(document.getElementById("height").value);
  
    console.log(baseValue);
    console.log(heightValue);
  
+   // validasi nilai base dan height jika kosong
    if (baseValue != "" && heightValue != "" && !isNaN(baseValue) && !isNaN(heightValue)) {
      console.log("Validation Succeed");
+
+     // perhitungan luas segitiga
      let area = (baseValue * heightValue) / 2;
      console.log(area);
-     document.getElementById("displayFormulaArea").textContent = `Area of Triangle: 0.5 x base x height`;
-     document.getElementById("displayValueArea").textContent = `Area of Triangle: 0.5 x ${baseValue} x ${heightValue}`;
-     document.getElementById("displayArea").textContent = `Area of Triangle: ${area}`;
+
+     // display hasil luas
+     document.getElementById("display-formula-area").textContent = `Area of Triangle: 0.5 x base x height`;
+     document.getElementById("display-value-area").textContent = `Area of Triangle: 0.5 x ${baseValue} x ${heightValue}`;
+     document.getElementById("display-area").textContent = `Area of Triangle: ${area}`;
    } else {
+      // validasi gagal
      console.log("Validation Failed");
      alert("Input Must be Filled Out");
    }
  }
 
+// fungsi reset inputan luas segitiga
+function resetArea() {
+   document.getElementById("display-formula-area").textContent = ``;
+   document.getElementById("display-value-area").textContent = ``;
+   document.getElementById("display-area").textContent = ``;
+}
+
+// fungsi validasi keliling
 function validationPerimeter() {
+   // ambil dan konversi nilai side a, b, dan c
    let aValue = parseFloat(document.getElementById("side-a").value);
    let bValue = parseFloat(document.getElementById("side-b").value);
    let cValue = parseFloat(document.getElementById("side-c").value);
@@ -48,84 +68,112 @@ function validationPerimeter() {
    console.log(bValue);
    console.log(cValue);
  
+   // validasi nilai side a, b, dan c jika kosong
    if (aValue != "" && bValue != "" && cValue != "" && !isNaN(aValue) && !isNaN(bValue) && !isNaN(cValue)) {
      console.log("Validation Succeed");
+
+     // perhitungan keliling segitiga
      let perimeter = (aValue + bValue + cValue);
      console.log(perimeter);
-     document.getElementById("displayFormulaPerimeter").textContent = `Perimeter of Triangle: a + b + c`;
-     document.getElementById("displayValuePerimeter").textContent = `Perimeter of Triangle: ${aValue} + ${bValue} + ${cValue}`;
-     document.getElementById("displayPerimeter").textContent = `Perimeter of Triangle: ${perimeter}`;
+
+     // display hasil keliling
+     document.getElementById("display-formula-perimeter").textContent = `Perimeter of Triangle: a + b + c`;
+     document.getElementById("display-value-perimeter").textContent = `Perimeter of Triangle: ${aValue} + ${bValue} + ${cValue}`;
+     document.getElementById("display-perimeter").textContent = `Perimeter of Triangle: ${perimeter}`;
    } else {
+      // validasi gagal
      console.log("Validation Failed");
      alert("Input Must be Filled Out");
    }
  }
+
+//fungsi reset inputan keliling segitiga
+function resetPerimeter() {
+   document.getElementById("display-formula-perimeter").textContent = ``;
+   document.getElementById("display-value-perimeter").textContent = ``;
+   document.getElementById("display-perimeter").textContent = ``;
+}
  
+// validasi digit input base
 document.getElementById("base").addEventListener('input', function displayChange() {
   console.log(this.value);
   let e = this.value;
   
+  // regex nilai yang bukan berada di rentang 0-9
   let chars = /^[0-9]+$/;
 
   if (chars.test(e)) {
      console.log("digits");
   } else {
+     // validasi gagal
      console.log("not digits");
      alert("Not Digits, Value Must be Positive!");
    }
 })
 
+// validasi digit input height
 document.getElementById("height").addEventListener('input', function displayChange() {
    console.log(this.value);
    let e = this.value;
    
+   // regex nilai yang bukan berada di rentang 0-9
    let chars = /^[0-9]+$/;
    
    if (chars.test(e)) {
       console.log("digits");
    } else {
+      // validasi gagal
       console.log("not digits");
       alert("Not Digits, Value Must be Positive!");
    }
 })
 
+// validasi digit input side-a
 document.getElementById("side-a").addEventListener('input', function displayChange() {
    console.log(this.value);
    let e = this.value;
    
+   // regex nilai yang bukan berada di rentang 0-9
    let chars = /^[0-9]+$/;
    
    if (chars.test(e)) {
       console.log("digits");
    } else {
+      // validasi gagal
       console.log("not digits");
       alert("Not Digits, Value Must be Positive!");
    }
 })
 
+// validasi digit input side-b
 document.getElementById("side-b").addEventListener('input', function displayChange() {
    console.log(this.value);
    let e = this.value;
    
+   // regex nilai yang bukan berada di rentang 0-9
    let chars = /^[0-9]+$/;
    
    if (chars.test(e)) {
       console.log("digits");
    } else {
+      // validasi gagal
       console.log("not digits");
       alert("Not Digits, Value Must be Positive!");
    }
 })
 
+// validasi digit input side-c
 document.getElementById("side-c").addEventListener('input', function displayChange() {
    console.log(this.value);
    let e = this.value;
    
+   // regex nilai yang bukan berada di rentang 0-9
    let chars = /^[0-9]+$/;
    
    if (chars.test(e)) {
       console.log("digits");
    } else {
+      // validasi gagal
       console.log("not digits");
       alert("Not Digits, Value Must be Positive!");
    }
